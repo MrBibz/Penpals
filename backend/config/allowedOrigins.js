6f521f3ee9ccf6174a05dc50928f77a9340d8fbd
mrbibz <blanco.bibeau@gmail.com>
2024-01-11 19:50:33 -0500

    Right before database setup

---
+++ backend/config/allowedOrigins.js
@@ -0,0 +1,6 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://penpals.web.app'
+];
+
+module.exports = allowedOrigins;
--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -5,11 +6,14 @@ const { logger } = require('./middleware/logger');
 const errorHandler = require('./middleware/errorHandler');
 const cookieParser = require('cookie-parser');
 const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
 const port = process.env.PORT || 3500;
+
+console.log(process.env.NODE_ENV);
 
 app.use(logger);
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
